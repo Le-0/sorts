be035675ba5aedcc74d2786e76f00db0eefbeb76
Lev Kolyadich <lkolyadich@gmail.com>
2020-07-08 10:58:34 +0000

    move definintion of sequence_type to another file

---
+++ source/sequence_type.hpp
@@ -0,0 +1,27 @@
+#pragma once 
+
+#include <exception>
+#include <string>
+
+using namespace std::literals;
+
+enum class sequence_type {
+	random,
+	sorted, 
+	reversed
+};
+
+std::string seqttos(sequence_type type)
+{
+	switch(type) {
+		case sequence_type::random:
+			return "random"s;
+		case sequence_type::sorted:
+			return "sorted"s;
+		case sequence_type::reversed:
+			return "reversed"s;
+		default:
+			throw std::invalid_argument{"argument is not a sequence_type variable"};
+	}
+}
+
